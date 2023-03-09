9c98c36a35353c5691319ffe813384ae998ad2b5
nefakemorfy <125242983+nefakemorfy@users.noreply.github.com>
2023-03-09 10:55:14 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,7 +10,17 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-
+            int[] array = new int[6];
+            for (int i = 0; i < 6; i++)
+            {
+                Console.Write($"array[{i}]=");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine($"array[{i}]={array[i]}");
+            }
+            Console.Read();
         }
     }
 }
