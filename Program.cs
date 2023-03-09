d1c2af663640a6566ee5b39f8f987947812e86b5
nefakemorfy <125242983+nefakemorfy@users.noreply.github.com>
2023-03-09 12:16:34 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -30,6 +30,16 @@ namespace ConsoleApp1
             {
                 Console.WriteLine($"array[{i}]={array[i]}");
             }
+            Console.Write("n=");
+            byte n = byte.Parse(Console.ReadLine());
+            double [] array = new double[n];
+            Random rnd = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = -50 + (50 - (-50)) * rnd.NextDouble();
+                Console.WriteLine($"array [{i}]={array[i]}");
+            }
+            Console.WriteLine($"array[0]={array[0]}\narray[{array.Length-1}]={array[array.Length-1]}");
             Console.Read();
         }
     }
